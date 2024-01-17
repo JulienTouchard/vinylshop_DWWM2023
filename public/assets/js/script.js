c00ac5b070ca60ce93f2341b44aea37dc07b5d61
JulienTouchard <julien.touchard@free.fr>
2024-01-17 22:55:13 +0100

    slider add in app_home

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,8 @@
 import {menuEntries} from "./modules/menuEntries.js"
 import { createEntry } from "./modules/menu.js";
 import { burgerManager } from "./modules/burgerManager.js";
+
+
 console.dir(menuEntries);
 const navMenu = document.querySelector("#navMenu");
 createEntry( menuEntries,navMenu );
